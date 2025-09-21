d27c3fd61fe4a1c2c8e54c613b2c3020d85c3bd5
mukeshrawatmkr <swissmukeshrawat@gmail.com>
2025-09-22 00:32:05 +0530

    covered 2nd lecture

--- 1. Hands on Java/2. Core Java/src/JAVA CORE README.md
+++ 1. Hands on Java/2. Core Java/src/JAVA CORE README.md
@@ -1,7 +1,9 @@
 # JAVA Core
 
-**1. Intro to Programming And Hello World** *recorded on -> 25 May 2023*
+**1. Intro to Programming And Hello World** 
+    *recorded on -> 25 May 2023*
 
 **2. Operators & conditional Statements**
+    *recorded on -> 27 May 2023*
 
 **3. **
---
+++ 1. Hands on Java/2. Core Java/src/OperatorsAndConditionalStatements/Conditions.java
@@ -0,0 +1,16 @@
+package OperatorsAndConditionalStatements;
+
+
+//5th file
+public class Conditions {
+    public static void main(String[] args){
+
+        //1. if, if-else, if-else-if ladder.
+        //2. switch
+        //3. Ternary operator
+        //(*)4. Pattern Matching for switch & instanceof (Java 16+)
+
+
+    }
+
+}
